8b70e18c2472d52ddb95ceda8eb4dbd20604907c
danielle <djcasquejo@gmail.com>
2025-02-24 22:36:01 -0800

    commit with server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const app = require('./backend/app');
-// const { debug } = require('console');
 const debug = require("debug")("node-angular");
 
 function normalizePort(val) {
@@ -12,22 +11,22 @@ function normalizePort(val) {
         return port;
     }
     return false;
-};
-
+}
 
 const onError = error => {
-    if (error.syscall !== "listen"){
+    if (error.syscall !== "listen") {
         throw error;
     }
 
-    const bind = typeof addr === "string" ? "pipe" + addr: "port" + port;
-    switch (error.code){
-        case "EACCESS":
-            console.error(bind + "requires elevated privileges");
+    const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+
+    switch (error.code) {
+        case "EACCES":  
+            console.error(bind + " requires elevated privileges");
             process.exit(1);
             break;
         case "EADDRINUSE":
-            console.error(bind + "is already in user");
+            console.error(bind + " is already in use"); 
             process.exit(1);
             break;
         default:
@@ -36,12 +35,12 @@ const onError = error => {
 };
 
 const onListening = () => {
-    const addr = http.Server.address();
-    const bind = typeof addr === "string" ? "pipe" + addr: "port" + port;
-    debug("Listening on" + bind);
+    const addr = server.address(); 
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+    debug("Listening on " + bind);
 };
 
-const port = normalizePort (process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || 3000);
 app.set("port", port);
 
 const server = http.createServer(app);
